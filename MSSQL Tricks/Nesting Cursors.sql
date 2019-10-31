3dbf3d3e1030049a4e991545b0231e0b7c6a2b18
tallrain <tallrain@gmail.com>
2019-10-31 09:35:14 +0800

    Update Nesting Cursors.sql

--- MSSQL Tricks/Nesting Cursors.sql
+++ MSSQL Tricks/Nesting Cursors.sql
@@ -1,26 +1,25 @@
 -- Normally, we use system variable @@FETCH_STATUS to check Cursor loop
-	OPEN myCursor;
-	FETCH NEXT FROM myCursor INTO @myVar;
-	WHILE @@FETCH_STATUS = 0
-  BEGIN
-    --DO somthing;
-  END;
+OPEN myCursor;
+FETCH NEXT FROM myCursor INTO @myVar;
+WHILE @@FETCH_STATUS = 0
+BEGIN
+  --DO somthing;
+END;
   
 - But @@FETCH_STATUS is global variable
 -- So if we use another Cursor within the loop, when inner loop is completed, @@FETCH_STATUS = -1, so the outer loop will also be closed.
 -- Fixing method is to use local variable instead of @@FETCH_STATUS
-	OPEN myCursor;
-	FETCH NEXT FROM myCursor INTO @myVar;
-  SET @Outer_loop = @@FETCH_STATUS;
-	WHILE @Outer_loop = 0
+OPEN myCursor;
+FETCH NEXT FROM myCursor INTO @myVar;
+SET @Outer_loop = @@FETCH_STATUS;
+WHILE @Outer_loop = 0
+BEGIN
+  --inner loop here
+  OPEN myCursor2;
+  FETCH NEXT FROM myCursor2 INTO @myVar2;
+  SET @Inner_loop = @@FETCH_STATUS;
+  WHILE @Inner_loop = 0
   BEGIN
-    --inner loop here
-    	OPEN myCursor2;
-	    FETCH NEXT FROM myCursor2 INTO @myVar2;
-	    SET @Inner_loop = @@FETCH_STATUS;
-	    WHILE @Inner_loop = 0
-      BEGIN
-        --Inner loop function
-      END
+    --Inner loop function
   END
- 
+END
