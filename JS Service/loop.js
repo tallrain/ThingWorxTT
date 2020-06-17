dd55365b28c3233eef0819f8de70aaca7190ad02
tallrain <tallrain@gmail.com>
2020-06-17 11:27:25 +0800

    Create loop.js

---
+++ JS Service/loop.js
@@ -0,0 +1,15 @@
+while(condition) {
+  // command
+}
+
+do {
+  // command
+} while(condition);
+
+var i;
+for(i = 0; i < 5; i++) {
+  // command
+}
+
+break; // end the cycle
+continue; // jump the begining of cycle
