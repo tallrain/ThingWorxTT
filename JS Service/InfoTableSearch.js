bb683055331c655dedfaa8b84f9e8706eb8b662d
tallrain <tallrain@gmail.com>
2020-07-01 11:04:35 +0800

    Update InfoTableSearch.js

--- JS Service/InfoTableSearch.js
+++ JS Service/InfoTableSearch.js
@@ -1,19 +1,19 @@
 var params75 = {
-fieldName: "name" /* STRING */,
-isCaseSensitive: false /* BOOLEAN */,
-t: outputTable /* INFOTABLE */,
-value: divisionName /* STRING */
+  fieldName: "name" /* STRING */,
+  isCaseSensitive: false /* BOOLEAN */,
+  t: outputTable /* INFOTABLE */,
+  value: divisionName /* STRING */
 };
 var divisionNameFound = Resources["InfoTableFunctions"].EQFilter(params75).rows.length;		
-  if (divisionNameFound === 0) {
-      outputTable.AddRow({
-          from:"",
-          to:divisionName,
-          name:divisionName,
-          type:"Division",
-          displayName:divisionName,
+if (divisionNameFound === 0) {
+  outputTable.AddRow({
+    from:"",
+    to:divisionName,
+    name:divisionName,
+    type:"Division",
+    displayName:divisionName,
     sort:divisionName,
     tabValue:"",
     standAlone:0
-    });     
-  }	
+  });     
+}	
