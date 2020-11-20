6dc911f6cf948baa0cfc8b3355246b286862beef
tallrain <tallrain@gmail.com>
2020-11-20 11:07:24 +0800

    Update Rebuild all indexes.sql

--- MSSQL Tools/Rebuild all indexes.sql
+++ MSSQL Tools/Rebuild all indexes.sql
@@ -1,8 +1,12 @@
 Declare @getTables CURSOR;
 Declare @TableName varchar(255);
 Declare @command nvarchar(4000);
-PRINT(GETDATE());
+DECLARE @DT NVARCHAR(50); 	-- Datetime
+
+SELECT @DT = CONVERT(NVARCHAR, GETDATE(), 121);	--YYYY-MM-DD HH:MI:SS
+PRINT(@DT);
 PRINT('Rebuilding Index started.');
+PRINT("");
 SET @getTables = CURSOR for SELECT name FROM sys.objects WHERE type = (N'U');
 
 OPEN @getTables;
@@ -10,18 +14,21 @@ OPEN @getTables;
 	WHILE @@FETCH_STATUS = 0
 	BEGIN;
 		--set @command = N'ALTER INDEX ALL ON ' + @TableName + N' REORGANIZE;';
-		set @command = N'ALTER INDEX ALL ON ' + @TableName + N' REBUILD;';
-		PRINT @command;
+		SELECT @DT = CONVERT(NVARCHAR, GETDATE(), 121);	--YYYY-MM-DD HH:MI:SS
+		PRINT(@DT);				
+		set @command = N'ALTER INDEX ALL ON ' + @TableName + N' REBUILD WITH (ONLINE=ON);';
+		PRINT(@command);
+		PRINT("");
 		BEGIN TRY  
 			EXEC (@command);
 		END TRY  
 		BEGIN CATCH  
 			PRINT 'CATCHED!';
 		END CATCH;  
-
 		FETCH NEXT FROM @getTables into @TableName;
 	END;
 CLOSE @getTables;
 DEALLOCATE @getTables; 
-PRINT(GETDATE());
+PRINT(@DT);
+PRINT('Rebuilding Index started.');
 PRINT('Rebuilding Index completed.');
